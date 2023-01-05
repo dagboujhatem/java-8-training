ab2b15e09acdceb6ec4cccb5c0b7e8603091d47b
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2023-01-06 00:15:38 +0100

    Expressions lambda

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Printer printer = new Printer() {
-            @Override
-            public void print(String message) {
-                System.out.println(message);
-            }
-        };
+        Printer printer = (String message) -> System.out.println(message);
         printer.print("Hello world!");
     }
 }
