f321acd65c0d68686cc6e6ab1a2fab26fc5001a6
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2023-01-06 00:05:58 +0100

    Functional interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Printer printer = new ConsolePrinter();
+        printer.print("Hello world!");
     }
 }
