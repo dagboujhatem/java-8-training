fbf6fd0b1ed11e1bb69f42d8d44def55cf4c8046
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2023-01-06 00:32:15 +0100

    Expressions lambda et variables

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
+    static String prefix = "Message : ";
+    String supparateur;
+
+    void test(String msg){
+        // accès a un attribut en utilisant this
+        Printer printer = message -> System.out.println(Main.prefix + this.supparateur + message);
+        printer.print(msg);
+    }
     public static void main(String[] args) {
-        Printer printer = message -> System.out.println(message);
-        printer.print("Hello world!");
+        String suffix = " ?";
+        // les attributs de classe
+        Main main = new Main();
+        main.supparateur = " => ";
+        // static attribute
+        // class attribute
+        // Simple variable
+        Printer printer = message -> System.out.println(Main.prefix + main.supparateur + message+suffix);
+        printer.print("Hello world");
+
+        // Example 2:  access using this keyword
+        main.test("hello");
+
+
     }
 }
