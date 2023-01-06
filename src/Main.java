086311e5f7a7d618a03fed7e2a631b8a57124cbc
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2023-01-06 02:19:13 +0100

    Interface "Predicate"

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,14 @@
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.IntSupplier;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class Main {
     public static void main(String[] args) {
-        // create a Function (Supplier & Consumer in the same time)
-        Function<String,Integer> function = (param) -> Integer.parseInt(param);
-        System.out.println(function.apply("34"));;
-
-        Function<String, Character> getFirstChar = (str) -> str.charAt(0);
-        System.out.println(getFirstChar.apply("Hatem"));
+        //  Interface "Predicate"
+        Predicate<String> hasLengthEven = (str) -> str.length() % 2 == 0;
+        System.out.println(hasLengthEven.test("hello")); // Output false
+        System.out.println(hasLengthEven.test("hello!")); // Output true
 
     }
 }
