f35438acbedab82d649100565a027bc71df75cdc
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2023-01-06 02:45:55 +0100

    Interface "UnaryOperator"

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,12 @@ import java.util.function.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Interface "BinaryOperator"
-        BinaryOperator<Integer> multiply = (a,b) -> a*b;
-        Function<Integer, Integer> multiplyBy10 = (x) -> multiply.apply(x,10);
-        // Test
-        System.out.println(multiply.apply(5,4));
-        System.out.println(multiplyBy10.apply(5));
+        // Interface "UnaryOperator"
+        UnaryOperator<Integer> cube = a -> a*a*a;
+        UnaryOperator<Integer> increment = a -> a+1;
 
-        var result = multiply.andThen(multiplyBy10).apply(5,6);
+        // Test
+        var result = cube.andThen(increment).apply(5);
         System.out.println(result);
 
     }
