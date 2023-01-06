24fdc899d4e14ed604e6e7b5647d859713176021
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2023-01-06 01:12:56 +0100

    Interface "Consumer"

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,21 @@
-public class Main {
-    static void afficher(String msg){
-        System.out.println(msg);
-    }
+import java.util.Arrays;
+import java.util.List;
 
-    void affichage(String msg){
-        System.out.println(msg);
-    }
-    Main (){
-    }
-    Main(String msg){
-        System.out.println(msg);
-    }
+public class Main {
     public static void main(String[] args) {
 
-        Printer printer1 = message -> System.out.println( message);
-        // 2nd way : using method reference
-        // syntax => Class::method
-        Printer printer2 = System.out::println;
+        List<String> list = Arrays.asList("Monday", "Tuesday", "Sunday");
+        // imperative approach (using imperative programming)
+        // C'est-à-dire lorsque tu programme une solution, tu dois dire comment la faire (itérer le tableau)
+        for (String day: list) {
+            System.out.println(day);
+        }
 
-        printer1.print("Bonjour tout le monde.");
-        printer2.print("Good bye.");
+        // Declarative approach (using declarative programming)
+        // C'est-à-dire lorsque tu programme une solution, dire à Java ce nous voudrons faire s ns le dire comment la faire
+        // c'est à lui de savoir comment faire les choses (sans itérer le tableau)
+        list.forEach(day -> System.out.println(day));
+        list.forEach(System.out::println);
 
-        // Example 3: using static method
-        Printer printer3 = Main::afficher;
-        printer3.print("Print using method référence (static method).");
-
-        // Example 4: using a method of a class
-        Main main = new Main();
-        Printer printer4 = main::affichage;
-        printer4.print("Print using method référence (method of classe).");
-
-        // Example 5: reference of a constructor
-        Printer printer5 = Main::new;
-        printer5.print("Print using method référence (constructor of classe).");
     }
 }
