836ef1c84928cb4fc5a1b6c33e5d2f228e966534
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2023-01-06 01:56:14 +0100

    Interface "Function"

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,18 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-        // create a Supplier
-        Supplier<Integer> supplier = ()-> LocalDateTime.now().getYear();
-        System.out.println("Years : " + supplier.get());
+        // create a Function (Supplier & Consumer in the same time)
+        Function<String,Integer> function = (param) -> Integer.parseInt(param);
+        System.out.println(function.apply("34"));;
 
-        IntSupplier supplier2 = ()-> LocalDateTime.now().getMinute();
-        System.out.println("Minutes : " + supplier2.getAsInt());
+        Function<String, Character> getFirstChar = (str) -> str.charAt(0);
+        System.out.println(getFirstChar.apply("Hatem"));
 
     }
 }
