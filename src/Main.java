93326d59bef1e596f9087e04de9e80a1b37dfacf
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2023-01-06 01:36:59 +0100

    Enchaînement des consumers

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("Monday", "Tuesday", "Sunday");
-        // imperative approach (using imperative programming)
-        // C'est-à-dire lorsque tu programme une solution, tu dois dire comment la faire (itérer le tableau)
-        for (String day: list) {
-            System.out.println(day);
-        }
+        // Create Consumers
+        Consumer<String> print = System.out::println;
+        Consumer<String> printLowerCase = item -> System.out.println(item.toLowerCase());
+        Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase());
 
-        // Declarative approach (using declarative programming)
-        // C'est-à-dire lorsque tu programme une solution, dire à Java ce nous voudrons faire s ns le dire comment la faire
-        // c'est à lui de savoir comment faire les choses (sans itérer le tableau)
-        list.forEach(day -> System.out.println(day));
-        list.forEach(System.out::println);
+        // Chaining Consumers
+        list.forEach(print.andThen(printLowerCase).andThen(printUpperCase));
 
     }
 }
