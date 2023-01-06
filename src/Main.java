2e350f1d46cd06930fd53c10f6d9437b171dc6f4
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2023-01-06 02:28:05 +0100

    Combinaison des prédicats

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,19 @@ import java.util.function.*;
 public class Main {
     public static void main(String[] args) {
         //  Interface "Predicate"
-        Predicate<String> hasLengthEven = (str) -> str.length() % 2 == 0;
-        System.out.println(hasLengthEven.test("hello")); // Output false
-        System.out.println(hasLengthEven.test("hello!")); // Output true
+        Predicate<String> hasLeftParenthesis = (str) -> str.startsWith("(");
+        Predicate<String> hasRightParenthesis = (str) -> str.endsWith(")");
+        // && AND logic
+        Predicate<String> hasParentheses = hasLeftParenthesis.and(hasRightParenthesis);
+        // || OR logic
+        Predicate<String> hasAtLeastParenthesis = hasLeftParenthesis.or(hasRightParenthesis);
+        // ! NOT logic
+        Predicate<String> negate = hasAtLeastParenthesis.negate();
+
+        // Test
+        System.out.println(hasAtLeastParenthesis.test("(Hello"));
+        System.out.println(hasParentheses.test("(Hello"));
+        System.out.println(negate.test("(Hello"));
 
     }
 }
